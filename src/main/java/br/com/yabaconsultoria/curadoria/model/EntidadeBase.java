4209fb508526a6a7c6d36c1b29e44aa0504ff866
Lucas Copque <copque.faculdade@gmail.com>
2020-09-02 14:50:40 -0300

    adiciona entidade base

---
+++ src/main/java/br/com/yabaconsultoria/curadoria/model/EntidadeBase.java
@@ -0,0 +1,44 @@
+package br.com.yabaconsultoria.curadoria.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Objects;
+
+/**
+ * @author Lucas Copque
+ * @version 1.0
+ * @since 02/09/2020
+ */
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+public class EntidadeBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    public EntidadeBase(){
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EntidadeBase that = (EntidadeBase) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
