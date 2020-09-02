23686f33f183b4f069a6f4e30f19b02f032b788c
Lucas Copque <copque.faculdade@gmail.com>
2020-09-02 15:27:44 -0300

    adiciona anotações para setup de arquivos no bd

--- src/main/java/br/com/yabaconsultoria/curadoria/model/Arquivo.java
+++ src/main/java/br/com/yabaconsultoria/curadoria/model/Arquivo.java
@@ -1,17 +1,25 @@
 package br.com.yabaconsultoria.curadoria.model;
 
 import br.com.yabaconsultoria.curadoria.enumerated.TipoArquivo;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Transient;
+import javax.persistence.*;
 
 /**
  * @author Lucas Copque
  * @version 1.0
  * @since 02/09/2020
  */
+@Entity
+@DynamicUpdate
+@Getter
+@Setter
+@ToString
+@Table(name = "arquivo_table")
 public class Arquivo extends EntidadeBase{
 
     private String nome;
