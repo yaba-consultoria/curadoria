16e210faaf899357e62c39369f3fec65227c34dc
Lucas Copque <copque.faculdade@gmail.com>
2020-09-02 15:23:31 -0300

    adiciona model de localidade

---
+++ src/main/java/br/com/yabaconsultoria/curadoria/model/Localidade.java
@@ -0,0 +1,27 @@
+package br.com.yabaconsultoria.curadoria.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author Lucas Copque
+ * @version 1.0
+ * @since 02/09/2020
+ */
+@Entity
+@DynamicUpdate
+@Getter
+@Setter
+@ToString
+@Table(name = "localidade_table")
+public class Localidade extends EntidadeBase{
+
+    private String cidade;
+
+    private String estado;
+}
