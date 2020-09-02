904bec38d7375e65b513e41d00d2d9ddd72d1636
Lucas Copque <copque.faculdade@gmail.com>
2020-09-02 19:50:00 -0300

    adiciona repository e service de empresa

---
+++ src/main/java/br/com/yabaconsultoria/curadoria/service/EmpresaService.java
@@ -0,0 +1,32 @@
+package br.com.yabaconsultoria.curadoria.service;
+
+import br.com.yabaconsultoria.curadoria.model.Empresa;
+import br.com.yabaconsultoria.curadoria.repository.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Lucas Copque
+ * @version 1.0
+ * @since 02/09/2020
+ */
+@Service
+public class EmpresaService {
+
+    private EmpresaRepository empresaRepository;
+
+    @Autowired
+    public EmpresaService(EmpresaRepository empresaRepository){
+        this.empresaRepository = empresaRepository;
+    }
+
+    public List<Empresa> findAll(){
+        return this.empresaRepository.findAll();
+    }
+
+    public Long count(){
+        return this.empresaRepository.count();
+    }
+}
