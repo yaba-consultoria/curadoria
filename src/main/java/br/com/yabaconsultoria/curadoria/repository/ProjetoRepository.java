6313adf92b05ca33518ab281b8ffe15f60178f8d
Lucas Copque <copque.faculdade@gmail.com>
2020-09-02 15:29:58 -0300

    adiciona repository de projeto

---
+++ src/main/java/br/com/yabaconsultoria/curadoria/repository/ProjetoRepository.java
@@ -0,0 +1,14 @@
+package br.com.yabaconsultoria.curadoria.repository;
+
+import br.com.yabaconsultoria.curadoria.model.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Lucas Copque
+ * @version 1.0
+ * @since 02/09/2020
+ */
+@Repository
+public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
+}
