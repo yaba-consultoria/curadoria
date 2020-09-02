b5bad4116a2dda90eebbcdd20ae6ce4294a007ee
Lucas Copque <copque.faculdade@gmail.com>
2020-09-02 19:49:34 -0300

    adiciona modelo de empresa

---
+++ src/main/java/br/com/yabaconsultoria/curadoria/model/Empresa.java
@@ -0,0 +1,29 @@
+package br.com.yabaconsultoria.curadoria.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author Lucas Copque
+ * @version 1.0
+ * @since 02/09/2020
+ */
+@Entity
+@DynamicUpdate
+@Getter
+@Setter
+@ToString
+@Table(name = "empresa_table")
+public class Empresa extends EntidadeBase {
+
+    private String nome;
+
+    @CNPJ
+    private String cnpj;
+}
