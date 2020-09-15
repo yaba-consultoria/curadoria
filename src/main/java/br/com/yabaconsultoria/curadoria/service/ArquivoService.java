e030d2c9ce9dc52c78ac80dee628aa17b9886091
Lucas Copque <copque.faculdade@gmail.com>
2020-09-15 14:48:58 -0300

    organiza e documenta o serviço de arquivos

--- src/main/java/br/com/yabaconsultoria/curadoria/service/ArquivoService.java
+++ src/main/java/br/com/yabaconsultoria/curadoria/service/ArquivoService.java
@@ -1,5 +1,6 @@
 package br.com.yabaconsultoria.curadoria.service;
 
+import br.com.yabaconsultoria.curadoria.handler.HandlerException;
 import br.com.yabaconsultoria.curadoria.model.Arquivo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import java.util.Objects;
 import java.util.UUID;
 
 /**
+ * Classe de serviço para os métodos de arquivo
+ *
  * @author Lucas Copque
  * @version 1.0
  * @since 03/09/2020
@@ -21,35 +24,74 @@ import java.util.UUID;
 public class ArquivoService {
 
     private LocalStorageService localStorageService;
+    private HandlerException handlerException;
 
     @Autowired
-    public ArquivoService(LocalStorageService localStorageService) {
+    public ArquivoService(LocalStorageService localStorageService, HandlerException handlerException) {
         this.localStorageService = localStorageService;
+        this.handlerException = handlerException;
     }
 
-    public void upload(HttpServletRequest request, Arquivo arquivo){
+    /**
+     * Método que realiza upload do arquivo
+     *
+     * @param request Objeto que contém informações da requisição
+     * @param arquivo Arquivo persistido
+     */
+    public void upload(HttpServletRequest request, Arquivo arquivo) {
         prepare(arquivo);
         this.localStorageService.upload(request, arquivo);
+        log.info("Upload realizado com sucesso {}", arquivo.getCaminhoAbsoluto());
     }
 
+    /**
+     * Método que deleta o arquivo
+     *
+     * @param arquivo Arquivo que deseja deletado
+     */
+    public void delete(Arquivo arquivo) {
+        this.localStorageService.delete(arquivo);
+        log.info("Arquivo deletado com sucesso {}", arquivo.getCaminhoAbsoluto());
+    }
 
-    private void prepare(Arquivo arquivo){
+    /**
+     * Método que prepara o arquivo para upload
+     *
+     * @param arquivo Arquivo que está sendo persistido
+     */
+    private void prepare(Arquivo arquivo) {
         arquivo.setNome(generateFileName(arquivo.getAnexo()));
         arquivo.setExtensao(getExtension(arquivo.getNome()));
         arquivo.setCaminhoRelativo("documentos/projetos/" + UUID.randomUUID());
         log.info("Preparando arquivo para upload ...");
     }
 
+    /**
+     * Método que gera nome único para o arquivo
+     *
+     * @param multiPart Objeto que contém informações do arquivo
+     * @return Retorna o nome gerado do arquivo
+     */
     private String generateFileName(MultipartFile multiPart) {
-        return new Date().getTime() + "-" + Objects.requireNonNull(multiPart.getOriginalFilename()).replace(" ", "_")
+        String arquivoNome = new Date().getTime() + "-" + Objects.requireNonNull(multiPart.getOriginalFilename()).replace(" ", "_")
                 .toLowerCase();
+        log.info("Gerando nome do arquivo ...");
+        return arquivoNome;
     }
 
-    private String getExtension(String fileName){
-        if(fileName.contains(".")){
-            return fileName.substring(fileName.lastIndexOf(".") + 1);
+    /**
+     * Método que captura a extensão do arquivo
+     *
+     * @param fileName Nome do arquivo
+     * @return Retorna a extensão do arquivo capturada
+     */
+    private String getExtension(String fileName) {
+        try {
+            if (fileName.contains(".")) return fileName.substring(fileName.lastIndexOf(".") + 1);
+            log.error("Falha ao extrair extensão do arquivo.");
+        } catch (Exception ex) {
+            log.warn("Falha ao capturar a extensão do arquivo {}", handlerException.buildMessage(ex));
         }
-        log.error("Falha ao extrair extensão do arquivo.");
-        throw new RuntimeException("Extensão não encontrada.");
+        return null;
     }
 }
