cf2b1be9a1d49216612d3a48b5d033b3bbca9aca
Lucas Copque <copque.faculdade@gmail.com>
2020-09-15 15:03:59 -0300

    organiza e documenta os serviços de controle de sessão

--- src/main/java/br/com/yabaconsultoria/curadoria/service/SessionService.java
+++ src/main/java/br/com/yabaconsultoria/curadoria/service/SessionService.java
@@ -1,38 +1,77 @@
 package br.com.yabaconsultoria.curadoria.service;
 
+import br.com.yabaconsultoria.curadoria.handler.HandlerException;
 import br.com.yabaconsultoria.curadoria.model.Usuario;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
+ * Classe de serviço que controla a sessão do usuário
+ *
  * @author Lucas Copque
  * @version 1.0
  * @since 04/09/2020
  */
 @Service
+@Slf4j
 public class SessionService {
 
-    private Map<String,Usuario> usersSession = new HashMap<>();
+    private HandlerException handlerException;
 
+    @Autowired
+    public SessionService(HandlerException handlerException) {
+        this.handlerException = handlerException;
+    }
+
+    /**
+     * Método que registra a sessão do usuário logado
+     *
+     * @param session Objeto que contém informações da sessão
+     * @param usuario Usuário que será controlado na sessão
+     */
     public synchronized void register(HttpSession session, Usuario usuario) {
-        usersSession.put(session.getId(), usuario);
         session.setAttribute("usuario", usuario);
+        log.info("Sessão iniciada para usuario {}", usuario.getEmail());
     }
 
-    public synchronized void logout(HttpSession session) {
-        usersSession.remove(session.getId());
-        session.invalidate();
+    /**
+     * Método que atualiza a quantidade de votos disponíveis na sessão do usuário para utilizar na categoria visualizada
+     *
+     * @param request Objeto que contém informações da request
+     * @param votes   Quantidade de votos disponíveis
+     */
+    public synchronized void updateVotes(HttpServletRequest request, Long votes) {
+        getUserLogged(request).setVotosDisponiveis(votes);
+        log.info("Votos disponíveis pelo usuário logado foram atualizados na sessão");
     }
 
-    public synchronized Usuario getUserLogged(HttpServletRequest request){
-        return (Usuario) request.getSession().getAttribute("usuario");
+    /**
+     * Método que finaliza a sessão do usuário
+     *
+     * @param request Objeto que contém informações da request
+     */
+    public synchronized void logout(HttpServletRequest request) {
+        try {
+            request.getSession().invalidate();
+        } catch (Exception ex) {
+            log.error("Falha ao finalizar a sessão do usuário");
+            log.error(handlerException.buildMessage(ex));
+        }
     }
 
-    public synchronized Integer getNumbersSessions(){
-        return usersSession.size();
+    /**
+     * Recupera o usuário logado na sessão
+     *
+     * @param request Objeto que contém informações da requisição
+     * @return Retorna o usuário recuperado na sessão
+     */
+    public synchronized Usuario getUserLogged(HttpServletRequest request) {
+        Usuario usuarioLogado = (Usuario) request.getSession().getAttribute("usuario");
+        log.info("Recuperando sessão do usuário {}", usuarioLogado.getEmail());
+        return usuarioLogado;
     }
 }
