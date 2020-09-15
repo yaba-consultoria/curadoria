1b839a183588d9af29a5fd002237aeb4d3630661
Lucas Copque <copque.faculdade@gmail.com>
2020-09-15 16:18:12 -0300

    organiza e documenta classe controller index

--- src/main/java/br/com/yabaconsultoria/curadoria/controller/HomeController.java
+++ src/main/java/br/com/yabaconsultoria/curadoria/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
+ * Classe controller por fazer a chamada da URI inicial
  * @author Lucas Copque
  * @version 1.0
  * @since 02/09/2020
